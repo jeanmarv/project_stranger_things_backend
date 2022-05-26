dc461976764207cd7ef05dc73c46bc1010a6ca51
Jean Marcel <jeanvanzella91@gmail.com>
2022-05-26 15:49:02 -0300

    setting upsidedown

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || 3000;
+const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE === 'true');
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
